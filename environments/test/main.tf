01a69c4ccc97270671ebb24510c684e431b138b5
Adrian IRIMUS <adrianirimus@gmail.com>
2026-04-03 15:36:26 +0300

    Add variables

--- environments/test/main.tf
+++ environments/test/main.tf
@@ -1,28 +1,41 @@
-resource "aws_vpc" "this" {
-  cidr_block = var.vpc_cidr
-  enable_dns_hostnames = true
-  tags = { Name = "drcarmen-vpc-${var.env}" }
+provider "aws" {
+  region = "eu-central-1"
 }
 
-resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.this.id
-  cidr_block = var.public_subnet_cidr
-  map_public_ip_on_launch = true
-  availability_zone       = var.az
-  tags = { Name = "drcarmen-public-sn-${var.env}" }
+provider "cloudflare" {
+  api_token = var.cloudflare_token
 }
 
-resource "aws_internet_gateway" "this" { vpc_id = aws_vpc.this.id }
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.this.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.this.id
-  }
+module "vpc" {
+  source             = "../../modules/vpc"
+  env                = "test"
+  az                 = "eu-central-1a"
+  vpc_cidr           = "10.0.0.0/16"
+  public_subnet_cidr = "10.0.1.0/24"
 }
 
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
+module "storage" {
+  source    = "../../modules/storage"
+  env       = "test"
+  random_id = "adrian-v1"
+}
+
+module "compute" {
+  source        = "../../modules/compute"
+  env           = "test"
+  vpc_id        = module.vpc.vpc_id
+  subnet_id     = module.vpc.public_subnet_id
+  bucket_arn    = module.storage.bucket_arn
+  my_ip         = var.my_ip_address
+  key_name      = var.ssh_key_name
+  deploy_ecs    = var.deploy_ecs
+  instance_type = "r5.large"
+}
+
+resource "cloudflare_record" "app_dns" {
+  zone_id = var.cloudflare_zone_id
+  name    = "staging"
+  value   = var.deploy_ecs ? "127.0.0.1" : module.compute.server_public_ip
+  type    = "A"
+  proxied = true
 }
