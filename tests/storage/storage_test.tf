e96d92b58b33e01a0f252cbf55674332c499a386
Adrian IRIMUS <adrianirimus@gmail.com>
2026-04-05 15:14:28 +0300

    add steps

--- tests/storage/storage_test.tf
+++ tests/storage/storage_test.tf
@@ -15,7 +15,6 @@ provider "aws" {
   skip_credentials_validation   = true
   skip_metadata_api_check       = true
   skip_requesting_account_id    = true
-  skip_get_ec2_platforms        = true
 }
 
 module "storage" {
